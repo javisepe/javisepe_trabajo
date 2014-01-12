f70e32d581c93413653a5b2bc89784e2a821752e
javisepe <javi_sepu@live.com>
2014-01-12 18:16:28 +0100

    test bench terminados
    terminado a falta de pruebas

--- Decoder_tb/decoder/decoder_tb.vhd
+++ Decoder_tb/decoder/decoder_tb.vhd
@@ -2,45 +2,38 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
-ENTITY decoder_decoder_tb_vhd_tb IS
-END decoder_decoder_tb_vhd_tb;
+ENTITY decoder_tb IS
+END decoder_tb;
 
-ARCHITECTURE behavior OF decoder_decoder_tb_vhd_tb IS 
+ARCHITECTURE behavior OF decoder_tb IS 
 
 	COMPONENT decoder
 	PORT(
-		code : IN  std_logic_vector(3 downto 0);
+		code : IN  std_logic_vector(2 downto 0);
 		led  : OUT std_logic_vector(6 downto 0)
 		);
 	END COMPONENT;
 
-	SIGNAL code : std_logic_vector(3 downto 0);
+	SIGNAL code : std_logic_vector(2 downto 0);
 	SIGNAL led  : std_logic_vector(6 downto 0);
 
 	TYPE vtest is record
-		 code : std_logic_vector(3 downto 0);
+		 code : std_logic_vector(2 downto 0);
 		 led  : std_logic_vector(6 downto 0);
 	END RECORD;
 
 	type vtest_vector is array (natural range <>) of vtest;
 
 	constant test: vtest_vector := (
-		(code => "0000", led => "0000001"),
-		(code => "0001", led => "1001111"),
-		(code => "0010", led => "0010010"),
-		(code => "0011", led => "0000110"),
-		(code => "0100", led => "1001100"),
-		(code => "0101", led => "0100100"),
-		(code => "0110", led => "0100000"),
-		(code => "0111", led => "0001111"),
-		(code => "1000", led => "0000000"),
-		(code => "1001", led => "0000100"),
-		(code => "1010", led => "1111110"),
-		(code => "1011", led => "1111110"),
-		(code => "1100", led => "1111110"),
-		(code => "1101", led => "1111110"),
-		(code => "1110", led => "1111110"),
-		(code => "1111", led => "1111110")
+		(code => "000", led => "0000001"),
+		(code => "001", led => "1001111"),
+		(code => "010", led => "0010010"),
+		(code => "011", led => "0000110"),
+		(code => "100", led => "1001100"),
+		(code => "101", led => "0100100"),
+		(code => "110", led => "0100000"),
+		(code => "111", led => "0001111")
+		
 	);
 
 BEGIN
